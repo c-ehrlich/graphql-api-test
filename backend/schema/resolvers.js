777407449746ed4b04e2dabed130eb98ad81b21b
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-22 15:14:05 +0200

    Create movie, movies, favoriteMovies queries

--- backend/schema/resolvers.js
+++ backend/schema/resolvers.js
@@ -1,10 +1,11 @@
-const { UserList } = require('../fakeData');
+const { MovieList, UserList } = require('../fakeData');
 const ld = require('lodash');
 
 // resolver args
 // 1. parent
 // 2. args
 const resolvers = {
+  // USER RESOLVERS
   Query: {
     users: () => {
       return UserList;
@@ -12,6 +13,27 @@ const resolvers = {
     user: (parent, args) => {
       const user = ld.find(UserList, { id: Number(args.id) });
       return user;
+    },
+
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = ld.find(MovieList, { name });
+      return movie;
+    },
+  },
+  // how do we know which movies are the favourite for each user
+  // note that our data in User specifies nothing about movies but we can still do this
+  User: {
+    favoriteMovies: () => {
+      return ld.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
